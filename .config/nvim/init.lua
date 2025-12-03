81d42d8d970ebefdbbddea1d64f0afe4b18e2cf7
perrorovic <perrorovic@gmail.com>
2025-12-03 21:24:23 +0700

    update: nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+vim.o.clipboard = "unnamedplus"
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight NormalNC guibg=none
+  highlight EndOfBuffer guibg=none
+  highlight SignColumn guibg=none
+  highlight StatusLine guibg=none
+  highlight CursorLine guibg=none
+  highlight CursorLineNr guibg=none
+]]
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
