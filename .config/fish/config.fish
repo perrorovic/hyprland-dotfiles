d20498614b07293da97c020a25bec390fce7c7f2
perrorovic <perrorovic@gmail.com>
2026-04-16 21:53:35 +0700

    update: keyd simple vim motions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,8 +12,8 @@ if status is-interactive
 end
 
 # List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
+alias l='eza -lh --icons=auto' # long list
+alias ls='eza -1 --icons=auto' # short list
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
